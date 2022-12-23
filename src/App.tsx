ec706876db958b5834b33427507131063e6038b4
Mart7anova <mart7anova@gmail.com>
2022-12-23 13:58:34 +0300

    add drag-and-drop

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useRef, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dragItem:  React.MutableRefObject<number | null> = useRef(null);
+    const dragOverItem = useRef(null);
+
+    const [list, setList] = useState(['Item 1','Item 2','Item 3','Item 4','Item 5','Item 6']);
+
+    const dragStart = (position: number) => {
+        dragItem.current = position
+    }
+    const dragEnter = (position: any) => {
+        dragOverItem.current = position;
+    };
+
+    const drop = () => {
+        const copyListItems = [...list];
+        const dragItemContent = copyListItems[Number(dragItem.current)];
+        copyListItems.splice(Number(dragItem.current), 1);
+        copyListItems.splice(Number(dragOverItem.current), 0, dragItemContent);
+        dragItem.current = null;
+        dragOverItem.current = null;
+        setList(copyListItems);
+    };
+
+    return (
+        <>
+            {
+                list &&
+                list.map((item, index) => (
+                    <div style={{backgroundColor:'lightblue', margin:'20px 25%', textAlign:'center', fontSize:'40px'}}
+                         key={index}
+                         onDragStart={() => dragStart(index)}
+                         onDragEnter={() => dragEnter(index)}
+                         onDragEnd={drop}
+                         onDragOver={(e) => e.preventDefault()}
+                         draggable>
+                        {item}
+                    </div>
+                ))}
+        </>
+    );
 }
 
 export default App;
